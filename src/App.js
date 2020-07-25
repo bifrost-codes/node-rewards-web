88eac3500f502b3f4b74e4ea343e28025b56ad2a
lurpis <hi@lurpis.com>
2020-07-25 12:00:14 +0800

    add order sort

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ class App extends React.Component {
       eosCountArray:[],
       eosBalanceArray:[],
       validatorArray:[],
-      validatorCount: 10,
+      validatorCount: 20,
     };
   }
 
